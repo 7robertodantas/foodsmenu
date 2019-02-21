2258d1a6d92f5e57cbc89b84908b6168f8d434d0
Roberto Dantas <7robertodantas@gmail.com>
2019-02-21 21:27:22 +0000

    Initial integration tests

---
+++ api/src/test/java/com/food/api/handler/MenuDescriptionHandlerTestIT.java
@@ -0,0 +1,42 @@
+package com.food.api.handler;
+
+import com.food.api.dto.*;
+import com.food.api.integration.IntegrationTestSupport;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+
+import java.util.List;
+
+import static com.food.core.utils.CollectionUtils.asSet;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+
+public class MenuDescriptionHandlerTestIT extends IntegrationTestSupport {
+
+    @Test
+    @DisplayName("Should create menu post - POST /menu/descriptions")
+    public void shouldCreateMenuDescription() {
+        List<ItemDto> itemDtos = singletonList(new ItemDto("X-Bacon", asList("Bacon", "Hamburguer", "Cheese"), asSet("10off", "3pay2hamburguer", "3pay2cheese")));
+        List<QuantitySaleDto> quantitySaleDtos = asList(new QuantitySaleDto("3pay2hamburguer", "For each 3 hamburguers pay only 2.", "Hamburguer", 3, 1),
+                new QuantitySaleDto("3pay2cheese", "For each 3 cheese pay only 2.", "Cheese", 3, 1));
+        List<CompositionSaleDto> compositionSaleDtos = singletonList(new CompositionSaleDto("10off", "10% OFF - Must have lettuce and not have bacon.", 0.1, asSet("Lettuce"), asSet("Bacon")));
+        MenuDescriptionDto.SalesDto salesDto = new MenuDescriptionDto.SalesDto(quantitySaleDtos, compositionSaleDtos);
+        List<IngredientDto> ingredientDtos = asList(new IngredientDto("Lettuce", 0.4),
+                new IngredientDto("Bacon", 2.0),
+                new IngredientDto("Hamburguer", 3.0),
+                new IngredientDto("Egg", 0.8),
+                new IngredientDto("Cheese", 1.5));
+
+        MenuDescriptionDto menuDescription = new MenuDescriptionDto("menu", itemDtos, salesDto, ingredientDtos);
+
+        web.post().uri("/menus/descriptions")
+                .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .body(BodyInserters.fromObject(menuDescription))
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+
+}
---
+++ api/src/test/java/com/food/api/integration/IntegrationTestSupport.java
@@ -0,0 +1,18 @@
+package com.food.api.integration;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+public abstract class IntegrationTestSupport {
+
+    @Autowired
+    protected WebTestClient web;
+
+}
