f75fab881bf6c93edc31353981720abc1dc27795
Roberto Dantas <7robertodantas@gmail.com>
2019-02-24 14:17:38 -0300

    Remove ingredient repository

--- api/src/main/java/com/food/api/repository/IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.food.api.repository;
-
-import com.food.api.dto.IngredientDto;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface IngredientRepository extends ReactiveCrudRepository<IngredientDto, String> {
-}
